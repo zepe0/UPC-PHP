9720ae8f2fb822410bfcc884810293656123cade
zepe <aitor.zepe@gmail.com>
2024-03-20 10:39:15 +0100

    first

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="es">
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Document</title>
+  </head>
+  <body>
+    <?php echo "<h1> Hello World </h1>" ?>
+  </body>
+</html>
